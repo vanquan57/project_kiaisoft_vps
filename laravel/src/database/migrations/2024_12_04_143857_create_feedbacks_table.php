a822e47fe46a9c6857cb6d60e551c5b323ecb1c5
Le Van Quan <kiaisoft.quan.le@gmail.com>
2024-12-04 15:11:24 +0700

    Completed creating migration file

---
+++ laravel/src/database/migrations/2024_12_04_143857_create_feedbacks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->text('content');
+            $table->tinyInteger('star')->default(0);
+            $table->tinyInteger('status')->default(0)->comment('0: Chờ phê duyệt, 1: Phê duyệt');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};
