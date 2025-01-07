bc3b0633a025918e814fc38a0bff8e61f12df8b8
Le Van Quan <kiaisoft.quan.le@gmail.com>
2025-01-07 16:23:14 +0700

    Project GD2: Install the library and delete unnecessary files

---
+++ vue-user/LibraBook/src/config/axios.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_URL_BACKEND,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+});
+
+axiosInstance.interceptors.request.use(
+    async config => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        if (config.data instanceof FormData) {
+            config.headers['Content-Type'] = 'multipart/form-data';
+        } else {
+            config.headers['Content-Type'] = 'application/json';
+        }
+
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        return Promise.reject(error.response);
+    }
+);
+
+export default axiosInstance;
