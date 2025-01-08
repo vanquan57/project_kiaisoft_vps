d5dc65dfa58e6c505185848f71630dc5d3b00e83
Le Van Quan <kiaisoft.quan.le@gmail.com>
2025-01-08 08:39:10 +0700

    Project GD2: Completed feature frontend interface login view

--- vue-user/LibraBook/src/router/index.js
+++ vue-user/LibraBook/src/router/index.js
@@ -14,6 +14,7 @@ import ContactView from '@/views/ContactView.vue';
 import DetailsView from '@/views/DetailsView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
 import { useAuthStore } from '@/stores/auth';
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -30,7 +31,10 @@ const router = createRouter({
                 {
                     path: 'auth/login',
                     name: 'auth-login',
-                    component: LoginView
+                    component: LoginView,
+                    meta: {
+                        isLogin: true
+                    }
                 },
                 {
                     path: 'auth/register',
